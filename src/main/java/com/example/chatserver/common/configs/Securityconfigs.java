4d84a5cc03d852119da74632d3ff2269d6481f16
aykwon <pj.aykwon@kalmate.net>
2025-05-02 17:57:42 +0900

    socket origin

--- src/main/java/com/example/chatserver/common/configs/Securityconfigs.java
+++ src/main/java/com/example/chatserver/common/configs/Securityconfigs.java
@@ -35,7 +35,7 @@ public class Securityconfigs {
                 .csrf(AbstractHttpConfigurer::disable) // csrf 비활성화 (보안 공격에대해 대비하지 않겠다.)
                 .httpBasic(AbstractHttpConfigurer::disable) // HTTP Basic 비활성화 (http basic => 보안 인증방법 중 하나.)  => 토큰 기반의 인증 처리 할거기 때문에.
                 // 특정 Url패턴에 대해선 허용. 나머진 인증하겠음.
-                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin").permitAll().anyRequest().authenticated())
+                .authorizeHttpRequests(a -> a.requestMatchers("/member/create", "/member/doLogin", "/connect").permitAll().anyRequest().authenticated())
                 //.authorizeHttpRequests(a -> a.anyRequest().permitAll())
                 .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션방식을 사용하지 않겠다라는 의미
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
@@ -45,7 +45,7 @@ public class Securityconfigs {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("*"));  // 모든 http메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*"));  // 모든 헤더값 허용
         configuration.setAllowCredentials(true);    // 자격증명허용
